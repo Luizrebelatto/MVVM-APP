9e42119f53a280a2d585876ead4c71cf7ab58859
Luizrebelatto <lbrebelatto@gmail.com>
2024-05-07 18:32:19 -0300

    Initial Commit

---
+++ primeiroAppMVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  primeiroAppMVVM
+//
+//  Created by Luiz Gabriel Rebelatto Bianchi on 07/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
